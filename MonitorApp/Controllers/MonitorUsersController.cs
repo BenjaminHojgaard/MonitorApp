501b153f3ebf3404396e3e0949a5024577490535
BenjaminHojgaard <benjaminoest@hotmail.com>
2020-01-19 12:37:21 +0100

    added more http methods

--- MonitorApp/Controllers/MonitorUsersController.cs
+++ MonitorApp/Controllers/MonitorUsersController.cs
@@ -20,48 +20,40 @@ namespace MonitorApp.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        private ApplicationUserManager _userManager;
-
         public MonitorUsersController()
         {
         }
 
-        public MonitorUsersController(ApplicationUserManager userManager,
-            ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
-        {
-            UserManager = userManager;
-            AccessTokenFormat = accessTokenFormat;
-        }
-
-        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
-
-        public ApplicationUserManager UserManager
-        {
-            get
-            {
-                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
-        }
-
+        // GET api/MonitorUsers
         [AllowAnonymous]
-        public async Task<IHttpActionResult> Post([FromBody] MonitorUser user)
+        public List<MonitorUser> Get()
+        {
+            var list = new List<MonitorUser>();
+            foreach (var item in db.MonitorUsers)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+
+        // GET api/MonitorUsers/5
+        [AllowAnonymous]
+        public async Task<MonitorUser> Get(int id)
+        {
+            var result = await db.MonitorUsers.FirstOrDefaultAsync(userID => userID.Id == id);
+            if (result == null)
+                return null;
+
+            return result;
+
+        }
+
+        // POST api/MonitorUsers
+        [AllowAnonymous]
+        public IHttpActionResult Post([FromBody] MonitorUser user)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-
-
-            var newuser = new ApplicationUser() { UserName = user.Email, Email = user.Email };
-
-            IdentityResult result = await UserManager.CreateAsync(newuser, user.Password);
-
-            if (!result.Succeeded)
-            {
-                return BadRequest("Something went wrong...");
-            }
 
             user.DateCreated = DateTime.Now;
 
@@ -69,6 +61,34 @@ namespace MonitorApp.Controllers
             db.SaveChanges();
             return StatusCode(HttpStatusCode.Created);
 
+        }
+
+        // PUT api/MonitorUsers/5
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> Put(int id, [FromBody] MonitorUser user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            var result = await db.MonitorUsers.FirstOrDefaultAsync(userID => userID.Id == id);
+            if(result == null)
+            {
+                return BadRequest("No object found with that ID.");
+            }
+
+            if (!(string.IsNullOrEmpty(user.Email)))
+                result.Email = user.Email;
+
+            if (!(string.IsNullOrEmpty(user.Country)))
+                result.Country = user.Country;
+
+            if (!(string.IsNullOrEmpty(user.Password)))
+                result.Password = user.Password;
+
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.OK);
         }
 
     }
